0cc027df8655cd420a42fac13b2efa451a1c5590
Luna Duclos <luna@psg.io>
2016-09-28 13:07:53 +0200

    Update docker image version to 0.2 with the addition of proper namespace support

--- build-container.sh
+++ build-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build -t palmstonegames/kube-cert-manager:0.1.1 -t palmstonegames/kube-cert-manager:latest $(dirname "$0")
-docker push palmstonegames/kube-cert-manager:0.1.1
+docker build -t palmstonegames/kube-cert-manager:0.2 -t palmstonegames/kube-cert-manager:latest $(dirname "$0")
+docker push palmstonegames/kube-cert-manager:0.2
 docker push palmstonegames/kube-cert-manager:latest
