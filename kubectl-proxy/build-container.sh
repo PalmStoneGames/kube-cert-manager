553edb7b330fcac359b3e70d5e1c83d967960404
Nan Liu <nan.liu@gmail.com>
2017-04-20 11:41:21 -0700

    Remove kubectl-proxy and bump alpine ver
    * Use hyperkube instead of custom kubectl proxy
    * Bump alpine version to 3.5

--- kubectl-proxy/build-container.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t palmstonegames/kubectl-proxy:1.4.0 -t palmstonegames/kubectl-proxy:latest $(dirname "$0")
-docker push palmstonegames/kubectl-proxy:1.4.0
-docker push palmstonegames/kubectl-proxy:latest
